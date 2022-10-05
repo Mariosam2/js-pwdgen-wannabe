af00d37f78cf7e9782360d1bab3b79441917b1e6
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-05 13:40:07 +0200

    using prompt to save the data and display the pw

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 
 
+const password = document.getElementById("pw");
 
+
+/* alert prompt */
+const userName = prompt("Qual'è il tuo nome?")
+const userSurname = prompt("Qual'è il tuo cognome?")
+const userColor = prompt("Qual'è il tuo colore preferito?")
+
+
+password.innerHTML = userName + userSurname + userColor + "22";
